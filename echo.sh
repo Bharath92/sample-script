8c10cd60f7fabba9c022be568ab45300dfaa51b2
Shrivara <shrivara.jois@gmail.com>
2016-07-18 19:15:49 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -5,6 +5,8 @@ ls /build/output
 echo "/previousOutput"
 echo "<========================>"
 ls /build/previousOutput
+cat /build/previousOutput/test.js
 echo "<========================>"
 
-echo "hello once" >> /build/output/test.js
+
+echo "hello once" >> /build/output/test2.js
