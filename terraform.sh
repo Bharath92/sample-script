ef7c770cb25f335cfe0472db891c3b681415fb52
Bharath Upadhya <1992bharath@gmail.com>
2017-04-26 11:05:49 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -3,7 +3,7 @@ node GET/sample-script/sample-script/extractAWSCreds.js $(cat GET/env-aws/aws.js
 
 echo "Copying previous state file"
 cp previousOutput/terraform.tfstate GET/sample-script/sample-script
-
+ 
 echo "Fetching terraform"
 wget https://releases.hashicorp.com/terraform/0.6.16/terraform_0.6.16_linux_386.zip
 apt-get install unzip
